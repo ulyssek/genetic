0179ed949ec6f260130dcbaf94ef4f600e25e098
uklatzmann <ulysseklatzmann@gmail.com>
2018-09-08 11:01:54 +0800

    simulation class in argument in Experiment class

--- experiment.py
+++ experiment.py
@@ -9,8 +9,8 @@ import numpy as np
 class Experiment():
 
 
-	def __init__(self,simulation_kwargs,altruism_activated,sample_size,simulation_step,studied_function):
-		self.altruism_activated = altruism_activated
+	def __init__(self,simulation_class,simulation_kwargs,sample_size,simulation_step,studied_function):
+		self.simulation_class = simulation_class
 		self.simulation_kwargs = simulation_kwargs
 		self.sample_size = sample_size
 		self.simulation_step = simulation_step
@@ -21,10 +21,9 @@ class Experiment():
 
 	def run(self):
 		for i in range(self.sample_size):
-			s = Simulation(kwargs=self.simulation_kwargs,altruism_activated=self.altruism_activated)
+			s = self.simulation_class(kwargs=self.simulation_kwargs)
 			for j in range(self.simulation_step):
 				s.run()
-			s.step()
 			self.result.append(self.studied_function(s))
 			
 		
